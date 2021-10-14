555a2c48545545c64a36137aee051100b04c291b
jinzhan <jinzhan@baidu.com>
2021-10-14 11:42:48 +0800

    test: add test for tagged template string

--- test/template.spec.js
+++ test/template.spec.js
@@ -1,7 +1,8 @@
 describe('[template]: ', () => {
-    it("template as static property", function () {
-        let MyComponent = defineComponent(() => {});
-        MyComponent.template = '<span title="{{color}}">{{color}}</span>';
+    it('template as static property', function () {
+        let MyComponent = defineComponent(() => {
+            template('<span title="{{color}}">{{color}}</span>');
+        });
         let myComponent = new MyComponent({data: {color: 'red'}});
 
         let wrap = document.createElement('div');
@@ -15,9 +16,10 @@ describe('[template]: ', () => {
         document.body.removeChild(wrap);
     });
 
-    it("template as static property in inherits component", function () {
-        let B = defineComponent(() => {});
-        B.template = '<b title="b">b</b>';
+    it('template as static property in inherits component', function () {
+        let B = defineComponent(() => {
+            template('<b title="b">b</b>');
+        });
 
         let MyComponent = function (option) {
             B.call(this, option);
@@ -37,6 +39,37 @@ describe('[template]: ', () => {
         expect(wrap.getElementsByTagName('u').length).toBe(1);
 
         b.dispose();
+        myComponent.dispose();
+        document.body.removeChild(wrap);
+    });
+
+
+    it('template as tagged template string', function () {
+        let MyComponent = defineComponent(() => {
+            const color1 = 'blue';
+            const color2 = 'grey';
+            template`
+                <div>
+                    <span title="${color1}">${color1}</span>
+                    <span title="{{color}}">{{color}}</span>
+                    <span title="${color2}">${color2}</span>
+                </div>
+            `;
+            data('color', 'red');
+        });
+        
+        let myComponent = new MyComponent();
+        let wrap = document.createElement('div');
+        document.body.appendChild(wrap);
+        myComponent.attach(wrap);
+
+        let span0 = wrap.getElementsByTagName('span')[0];
+        let span1 = wrap.getElementsByTagName('span')[1];
+        let span2 = wrap.getElementsByTagName('span')[2];
+        expect(span0.title).toBe('blue');
+        expect(span1.title).toBe('red');
+        expect(span2.title).toBe('grey');
+
         myComponent.dispose();
         document.body.removeChild(wrap);
     });
